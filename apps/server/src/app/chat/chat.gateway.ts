1010f83e81b330108350dba0d87996ce06169afe
Stanimir Ivanov <stanimir.ivanov@gmail.com>
2026-07-22 19:56:17 +0200

    [stanimirivanov/chat-hub-99#7] Add schema validation (#10)

--- apps/server/src/app/chat/chat.gateway.ts
+++ apps/server/src/app/chat/chat.gateway.ts
@@ -7,8 +7,17 @@ import {
   ConnectedSocket,
   MessageBody,
 } from '@nestjs/websockets';
+import { UsePipes, Logger } from '@nestjs/common';
 import { Server, Socket } from 'socket.io';
-import { Logger } from '@nestjs/common';
+import {
+  ChatMessageSchema,
+  ChatMessage,
+  JoinRoomSchema,
+  JoinRoom,
+  KickUserSchema,
+  KickUser,
+} from '@chat-hub-99/shared-schema';
+import { SchemaValidationPipe } from '../pipes/schema-validation.pipe';
 
 @WebSocketGateway({
   cors: {
@@ -30,11 +39,35 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   }
 
   @SubscribeMessage('chat')
+  @UsePipes(new SchemaValidationPipe(ChatMessageSchema))
   handleChatMessage(
     @ConnectedSocket() client: Socket,
-    @MessageBody() payload: unknown
+    @MessageBody() payload: ChatMessage
   ) {
-    this.logger.log(`Received 'chat' event from ${client.id}:`, payload);
+    this.logger.log(`Validated chat payload from ${client.id}:`, payload);
     this.server.emit('chat', payload);
+  }
+
+  @SubscribeMessage('join_room')
+  @UsePipes(new SchemaValidationPipe(JoinRoomSchema))
+  handleJoinRoom(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() payload: JoinRoom
+  ) {
+    this.logger.log(`User ${payload.user.userName} joined ${payload.roomName}`);
+    client.join(payload.roomName);
+    this.server.to(payload.roomName).emit('join_room', payload);
+  }
+
+  @SubscribeMessage('kick_user')
+  @UsePipes(new SchemaValidationPipe(KickUserSchema))
+  handleKickUser(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() payload: KickUser
+  ) {
+    this.logger.log(
+      `User ${payload.userToKick.userName} kicked from ${payload.roomName}`
+    );
+    this.server.to(payload.roomName).emit('kick_user', payload);
   }
 }
