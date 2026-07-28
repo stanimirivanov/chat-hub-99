7c1b322b1965d3433517d2af018de167e4f90fa7
Stanimir Ivanov <stanimr@protonmail.com>
2026-07-28 07:35:47 +0200

    [stanimirivanov/chat-hub-99#71] Refactor infrastructure lib

--- libs/infrastructure/message/src/index.ts
+++ libs/infrastructure/message/src/index.ts
@@ -1,2 +1,8 @@
 export { makeSupabaseMessageRepository } from './lib/supabase-message-repository';
+
+export {
+  type ChatHubSupabaseClient,
+  SupabaseMessageClientTag,
+} from './lib/supabase-message-client';
+
 export { SupabaseMessageRepositoryLayer } from './lib/supabase-message-repository.layer';
